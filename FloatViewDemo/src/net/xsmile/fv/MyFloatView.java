b99ca811fb11a71c35400ee6461533b4e6f5390e
hunter123321000 <hunter123321000@gmail.com>
2013-10-29 16:20:17 +0800

    第一次修改測試OK

---
+++ FloatViewDemo/src/net/xsmile/fv/MyFloatView.java
@@ -0,0 +1,82 @@
+package net.xsmile.fv;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class MyFloatView extends ImageView {
+	private float mTouchStartX, mTouchEndX;
+	private float mTouchStartY, mTouchEndY;
+	private float x;
+	private float y;
+
+	private WindowManager wm = (WindowManager) getContext()
+			.getApplicationContext().getSystemService("window");
+
+	// 此wmParams为获取的全局变量，用以保存悬浮窗口的属性
+	private WindowManager.LayoutParams wmParams = ((MyApplication) getContext()
+			.getApplicationContext()).getMywmParams();
+
+	public MyFloatView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		boolean b_click = false;
+		// 获取相对屏幕的坐标，即以屏幕左上角为原点
+		x = event.getRawX();
+		y = event.getRawY() - 25; // 25是系统状态栏的高度
+		Log.i("currP", "currX" + x + "====currY" + y);
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			// 获取相对View的坐标，即以此View左上角为原点
+			mTouchStartX = event.getX();
+			mTouchStartY = event.getY();
+//			Log.i("0.0", "111startX" + mTouchStartX + "====startY"
+//					+ mTouchStartY);
+
+			break;
+		case MotionEvent.ACTION_MOVE:
+//			Log.i("0.0", "222startX" + mTouchStartX + "====startY"
+//					+ mTouchStartY);
+			updateViewPosition();
+			break;
+
+		case MotionEvent.ACTION_UP:
+			mTouchEndX = event.getX();
+			mTouchEndY = event.getY();
+			if ((mTouchEndX == mTouchStartX) && (mTouchEndY == mTouchStartY)){
+				Log.i("0.0", "click");
+				b_click = true;
+			}else{
+				Log.i("0.0", "touch");
+				b_click = false;
+			}
+//			Log.i("0.0", "333startX" + mTouchStartX + "====startY"
+//					+ mTouchStartY);
+			updateViewPosition();
+			mTouchStartX = mTouchStartY = 0;
+			break;
+
+		}
+		if (b_click == false)
+			return true;
+		else
+			return false;
+	}
+
+	
+	private void updateViewPosition() {
+		// 更新浮动窗口位置参数
+		wmParams.x = (int) (x - mTouchStartX);
+		wmParams.y = (int) (y - mTouchStartY);
+		wm.updateViewLayout(this, wmParams);
+
+	}
+
+}
